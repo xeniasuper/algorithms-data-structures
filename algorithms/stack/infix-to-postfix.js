482bb3ec26cc80a593cf928e7a23c8df7b8593fc
xeniasuper <ichmalegern@gmail.com>
2019-04-14 14:31:02 +0300

    Add solution to the infixToPostfix problem

---
+++ algorithms/stack/infix-to-postfix.js
@@ -0,0 +1,77 @@
+"use strict";
+
+
+/**
+ * Converts from infix to postfix arithmetic notation
+ * @param str - arithmetic expression
+ * @return {string}
+ */
+function infixToPostfix(str) {
+    let output = "";
+
+    let operatorsPriorities = {
+        "+": 2,
+        "-": 2,
+        "*": 1,
+        "/": 1
+    };
+    let operatorsStack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (!operatorsPriorities.hasOwnProperty(str[i])) {
+            if (str[i] === "(") {
+                operatorsStack.push(str[i]);
+            } else if (str[i] === ")") {
+                let numToPop = 0;
+
+                for (let j = operatorsStack.length-1; j >= 0; j--) {
+                    if (operatorsStack[j] !== "(") {
+                        numToPop++;
+                    } else {
+                        numToPop++;
+                        break;
+                    }
+                }
+                for (let k = 0; k < numToPop; k++) {
+                    let operator = operatorsStack.pop();
+                    if (operator !== "(") {
+                        output += operator;
+                    }
+                }
+            } else {
+                output += str[i];
+            }
+        } else {
+            let lastOperator = operatorsStack[operatorsStack.length-1];
+
+            if (operatorsStack.length === 0 ||
+                operatorsStack.indexOf("(") !== -1 ||
+                operatorsPriorities[lastOperator] > operatorsPriorities[str[i]]) {
+                    operatorsStack.push(str[i]);
+            } else {
+                let numToPop = 0;
+                for (let j = operatorsStack.length-1; j >= 0; j--) {
+                    let operator = operatorsStack[j];
+                    if (operatorsStack[j] === "(" || operatorsStack[j] === ")") {
+                        break;
+                    } else if (operatorsPriorities[operator] <= operatorsPriorities[str[i]]) {
+                        numToPop++;
+                    }
+                }
+
+                for (let k = 0; k < numToPop; k++) {
+                    output += operatorsStack.pop();
+                }
+                operatorsStack.push(str[i]);
+            }
+        }
+    }
+
+    while (operatorsStack.length !== 0) {
+        output += operatorsStack.pop();
+    }
+
+    return output;
+}
+
+console.log(infixToPostfix("(2+3)*(4+2)"));
---
+++ data-structures/README.md
@@ -0,0 +1,2 @@
+# basic-data-structures
+My solutions to the problems on data structures from freeCodeCamp's coding interview prep section
