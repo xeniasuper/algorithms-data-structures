b44f8774956b13ac4585c6cc4cd30e66806c76cd
xeniasuper <ichmalegern@gmail.com>
2019-04-18 23:55:56 +0300

    Add more efficient solution to stockSpan

--- algorithms/real-world-algorithms/stockSpan.js
+++ algorithms/real-world-algorithms/stockSpan.js
@@ -18,3 +18,23 @@ function stockSpan(quotes) {
 }
 
 // console.log(stockSpan([7, 11, 8, 6, 4, 8, 9]));
+
+function stockSpanEffective(quotes) {
+    let days = [0];
+    let spans = [1];
+
+    for (let i = 1; i < quotes.length; i++) {
+        while (!(days.length === 0) && quotes[days[days.length-1]] <= quotes[i]) {
+            days.pop();
+        }
+        if (days.length === 0) {
+            spans[i] = i+1;
+        } else {
+            spans[i] = i-days[days.length-1];
+        }
+        days.push(i);
+    }
+    return spans;
+};
+
+// console.log(stockSpanEffective([7, 11, 8, 6, 4, 8, 9]));
